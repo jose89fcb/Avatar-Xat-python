fa9fc8ebd857b19e24ed7a875de0babf121daaf8
Jose89fcb <60312427+jose89fcb@users.noreply.github.com>
2022-10-25 03:03:33 +0200

    Add files via upload

---
+++ AvatarXat.py
@@ -0,0 +1,50 @@
+import json
+import requests
+import os
+import pathlib
+
+
+
+
+
+
+usuario_xat = input("Escribe el nombre de usuario de xat: ")
+
+
+
+response = requests.get(f"https://xat.com/web_gear/chat/profile2.php?n={usuario_xat}")
+
+
+
+
+
+#Programado Por Jose89fcb
+#Mi twitter: https://twitter.com/jose89fcb
+
+
+
+
+
+
+
+
+avatar = response.json()['Err']['Media']['avatar']
+
+
+nombre_local_imagen = f"{usuario_xat}.png"
+
+pathlib.Path(f"{usuario_xat}").mkdir(parents=True, exist_ok=True)
+os.chdir(os.path.join(os.getcwd(),f"{usuario_xat}")) 
+
+
+imagen = requests.get(avatar).content
+
+
+    
+   
+with open(nombre_local_imagen, 'wb') as handler:
+    handler.write(imagen)
+
+
+print(f"{avatar}")
+  
